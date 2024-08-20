cb5415cedea94d4a77e1cc8fdbc041ea4e15884c
rabongee <rmarkdehdals@gmail.com>
2024-08-20 12:25:40 +0900

    feat: 로그아웃 기능 구현
    로그아웃 기능을 구현하여 decorator처리
    index 상단에 navbar를 만들어 로그인페이지 이동, 로그아웃 할 수 있게 함

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_POST, require_http_methods
 
 # Create your views here.
 
 
+@require_http_methods(["GET", "POST"])
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
@@ -17,3 +20,10 @@ def login(request):
         "form": form
     }
     return render(request, "accounts/login.html", context)
+
+
+@require_POST
+def logout(request):
+    if request.method == "POST":
+        auth_logout(request)
+    return redirect('products:index')
