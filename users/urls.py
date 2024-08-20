db203f6738b484a8bc65854820f88e89df4ea8b3
rabongee <rmarkdehdals@gmail.com>
2024-08-20 19:34:39 +0900

    feat: user profile base 작성
    유저의 이름으로 url요청이 들어오면 유저 프로필 페이지로 가도록 만듬

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'users'
 urlpatterns = [
+    path("", views.users, name="users"),
+    path("<str:username>/", views.profile, name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def users(request):
+    return render(request, "users/users.html")
+
+def profile(request, username):
+    context = {
+        "username": username
+    }
+    return render(request, "users/profile.html", context)
