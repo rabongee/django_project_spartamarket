4ee8d2964ab58bcf33f9b834997910a343ddd9db
rabongee <rmarkdehdals@gmail.com>
2024-08-20 11:46:12 +0900

    feat: 로그인 기능 구현
    로그인 기능을 구현하며 index파일도 추가함

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("products:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/login.html", context)
--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
 app_name = 'products'
-urlpatterns = []
+urlpatterns = [
+    path('index/', views.index, name="index")
+]
