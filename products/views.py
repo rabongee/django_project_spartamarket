e003bcaf602c5917b05513fe044221f0b71d9f4b
rabongee <rmarkdehdals@gmail.com>
2024-08-22 12:36:46 +0900

    feat: 회원별 등록한 물품 보기 기능 추가
    프로필에 들어가서 그 회원이 올린 물품들을 볼 수 있는 기능을 넣음

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
+from .forms import ProductForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,8 +11,16 @@ def index(request):
 
 
 def products(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by("-pk")
     context = {
         "products": products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        "product": product,
+    }
+    return render(request, "products/product_detail.html", context)
