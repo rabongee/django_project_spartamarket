c5300f46b5a66df025d2ce4bfe1e6be458a5ec71
kinggoddino <swlee0609@gmail.com>
2024-08-22 12:10:39 +0900

    feat: New 등록 페이지 생성
    - new : 글 작성 페이지
    - create : 실제 글 추가 로직

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     path("", views.market, name="market"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 
 
@@ -7,8 +7,20 @@ def index(request):
 
 
 def market(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by("-pk")
     context = {
         "products": products,
     }
     return render(request, 'products/market.html', context)
+
+
+def new(request):
+    return render(request, "products/new.html")
+
+
+def create(request):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    price = request.POST.get("price")
+    Product.objects.create(title=title, content=content, price=price)
+    return redirect("products:market")
