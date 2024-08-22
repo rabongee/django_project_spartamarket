39162385ad401b3d9341ec0cccf9c77753852a3c
kinggoddino <swlee0609@gmail.com>
2024-08-22 11:58:12 +0900

    feat: Market 페이지 생성

--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Product
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def market(request):
+    products = Product.objects.all()
+    context = {
+        "products": products,
+    }
+    return render(request, 'products/market.html', context)
